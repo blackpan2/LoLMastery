115e175a756999e943fcb3a37ab1b6cc0fc31d3d
George Herde <blackpan2@gmail.com>
2016-04-28 17:29:45 -0400

    Updated files to include license information

--- static/mastery.js
+++ static/mastery.js
@@ -1,10 +1,25 @@
 /**
+ * Copyright 2016 George Herde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Created by Blackpan2 on 4/28/16.
  */
 function toUpper() {
     var str = document.getElementById("upper").innerHTML;
     var first = str.slice(0,1);
     var second = str.slice(1);
-    var res = first.toUpperCase() + second.toLowerCase();
-    document.getElementById("upper").innerHTML = res;
+    document.getElementById("upper").innerHTML = first.toUpperCase() + second.toLowerCase();
 }
