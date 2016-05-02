3b968da50e46fe3a5e6ff8125da8368d940f0875
George Herde <blackpan2@gmail.com>
2016-05-02 10:03:48 -0400

    Separator now is generated on the mastery numbers

--- static/mastery.js
+++ static/mastery.js
@@ -25,3 +25,44 @@ function toUpper() {
 }
 
 
+/* Function to format a number with separators. returns formatted number.
+ * num - the number to be formatted
+ * decimal point - the decimal point character. if skipped, "." is used
+ * sep - the separator character. if skipped, "," is used
+ */
+function FormatNumberBy3(num) {
+    // check for missing parameters and use defaults if so
+    var sep = ",";
+    var decimal_point = ".";
+    // need a string for operations
+    num = num.toString();
+    // separate the whole number and the fraction if possible
+    var a = num.split(decimal_point);
+    var x = a[0]; // decimal
+    var y = a[1]; // fraction
+    var z = "";
+    if (typeof(x) != "undefined") {
+        // reverse the digits. regexp works from left to right.
+        for (var i = x.length - 1; i >= 0; i--)
+            z += x.charAt(i);
+        // add separators. but undo the trailing one, if there
+        z = z.replace(/(\d{3})/g, "$1" + sep);
+        if (z.slice(-sep.length) == sep)
+            z = z.slice(0, -sep.length);
+        x = "";
+        // reverse again to get back the number
+        for (i = z.length - 1; i >= 0; i--)
+            x += z.charAt(i);
+        // add the fraction back in, if it was there
+        if (typeof(y) != "undefined" && y.length > 0)
+            x += decimal_point + y;
+    }
+    return x;
+}
+
+function FormatNumberBy3Loop() {
+    var formatList = document.getElementsByClassName("formatNumber3");
+    for (i = 0; i < formatList.length; i++) {
+        document.getElementsByClassName("formatNumber3")[i].innerHTML = FormatNumberBy3(formatList[i].innerHTML);
+    }
+}
